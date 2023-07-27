8a901a71e956291e3eebf408bdc59e9a79b7df9a
wendellopes <wendelopes@msn.com>
2023-07-27 15:21:25 -0300

    fix lint

--- spec/system/initial_pages_spec.rb
+++ spec/system/initial_pages_spec.rb
@@ -1,12 +1,14 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "InitialPages", type: :system do
+RSpec.describe 'InitialPages', type: :system do
   before do
     driven_by(:rack_test)
   end
 
-  describe "GET /index" do
-    it "returns http success" do
+  describe 'GET /index' do
+    it 'returns http success' do
       get root_url
       expect(response).to have_http_status(:success)
     end
